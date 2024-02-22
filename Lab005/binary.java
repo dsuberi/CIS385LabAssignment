910431d8600accc88d264c41773914242dc9f941
Suberi <69059493+dsuberi@users.noreply.github.com>
2024-02-22 13:39:02 -0500

    Commit Lab005 Folder by Devi

---
+++ Lab005/binary.java
@@ -0,0 +1,47 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+class BinarySearch{
+
+    int left, right, mid;
+
+    public int search(int[] a, int len, int x){
+        left = 0;
+        right = len - 1;
+
+        while(left <= right){
+            mid = (left+right)/2;
+
+            if(a[mid] == x)
+                return mid;
+            else if(x < a[mid])
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        return -1;
+    }
+}
+
+class binary{
+    public static void main(String[] args){
+        BinarySearch BS = new BinarySearch();
+
+        int[] arr = {5,3,7,1,8,9,32};
+
+        Arrays.sort(arr);
+
+        int search_value = 22;
+
+        int length = arr.length;
+        
+        int found = BS.search(arr, length, search_value);
+
+        if(found == -1)
+            System.out.print("Not Found");
+        else
+            System.out.print("Found at index: " + found);
+
+
+    }
+}
